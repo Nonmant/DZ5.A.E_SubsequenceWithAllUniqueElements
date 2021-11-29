fbdc672489269863eb909960158b251f069ebeba
Anton Bolotskih <89161424838@yandex.ru>
2021-11-29 23:57:19 +0300

    Checked both orders - left to right first and right to left first Same, wrong answer on 14

--- funcs.cpp
+++ funcs.cpp
@@ -3,20 +3,8 @@
 #include "funcs.h"
 #include <bits/stdc++.h>
 
-void parseFile(std::istream & input, std::ostream & output){
-
-    int n, k;
-    input >> n >> k;
-
-    std::vector<int> a(n);
-    std::copy_n(std::istream_iterator<int>(input),n,a.begin());
-
-    std::vector<unsigned long long int> treesColors(k+1);
-
-    for(auto i : a){
-        ++treesColors[i];
-    }
-
+std::pair<int, int> getRangeLeft(const std::vector<int> & a,
+                                 std::vector<unsigned long long int> treesColors){
     int iLeft = 0, iRight = a.size()-1;
     while (true){
         if(treesColors[a[iLeft]]>1){
@@ -34,6 +22,57 @@ void parseFile(std::istream & input, std::ostream & output){
         }
         --iRight;
     }
+    return {iLeft+1, iRight+1};
+}
 
-    output << iLeft+1 << ' ' << iRight+1;
+std::pair<int, int> getRangeRight(const std::vector<int> & a,
+                                 std::vector<unsigned long long int> treesColors){
+    int iLeft = 0, iRight = a.size()-1;
+    while (true) {
+        if(treesColors[a[iRight]]>1){
+            --treesColors[a[iRight]];
+        } else {
+            break;
+        }
+        --iRight;
+    }
+    while (true){
+        if(treesColors[a[iLeft]]>1){
+            --treesColors[a[iLeft]];
+        } else {
+            break;
+        }
+        ++iLeft;
+    }
+    return {iLeft+1, iRight+1};
+}
+
+void parseFile(std::istream & input, std::ostream & output){
+
+    int n, k;
+    input >> n >> k;
+
+    std::vector<int> a(n);
+    std::copy_n(std::istream_iterator<int>(input),n,a.begin());
+
+    std::vector<unsigned long long int> treesColors(k+1);
+
+    for(auto i : a){
+        ++treesColors[i];
+    }
+
+    auto [iLl, iRl] = getRangeLeft(a,treesColors);
+    auto [iLr, iRr] = getRangeRight(a,treesColors);
+
+    int iLeft, iRight;
+
+    if(iRl - iLl <= iRr - iLr){
+        iLeft = iLl;
+        iRight = iRl;
+    } else {
+        iLeft = iLr;
+        iRight = iRr;
+    }
+
+    output << iLeft << ' ' << iRight;
 }
