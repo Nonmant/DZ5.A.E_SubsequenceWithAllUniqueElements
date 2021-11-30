0b3ccfef1ef428a5d6c88d7a03bb54ddf3707639
Anton Bolotskih <89161424838@yandex.ru>
2021-11-30 22:26:29 +0300

    Time complexity N*log(K) Upd. Passed!

--- funcs.cpp
+++ funcs.cpp
@@ -12,18 +12,20 @@ void parseFile(std::istream & input, std::ostream & output){
     std::copy_n(std::istream_iterator<int>(input),n,a.begin()+1);
 
     std::vector<int> treesColors(k+1);
+    std::set<int> usedIndexes;
 
-    int colors = 0;
     int minLen = -1;
     int iLeft, iRight;
 
     for(int i=1; i<a.size(); ++i){
-        if(treesColors[a[i]] == 0)
-            ++colors;
+        if(treesColors[a[i]] != 0){
+            usedIndexes.erase(treesColors[a[i]]);
+        }
         treesColors[a[i]] = i;
-        if(colors == k){
+        usedIndexes.insert(i);
+        if(usedIndexes.size() == k){
 
-            int iL = *std::min_element(treesColors.begin()+1, treesColors.end());
+            int iL = *usedIndexes.begin();
             int len = i - iL + 1;
             if(len < minLen || minLen == -1){
                 minLen = len;
